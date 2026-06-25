efc7d26d9bc4d3f0f8bff762c4db05978f3c2834
XiuyuanJin <97435279+XiuyuanJin@users.noreply.github.com>
2026-06-25 16:09:21 +0800

    Add files via upload

--- code.for.benchmarking/run_CellSIUS.R
+++ code.for.benchmarking/run_CellSIUS.R
@@ -1,6 +1,6 @@
 setwd("/home/jinxiuyuan/Proj_scCellFishing/")
 
-library(aKNNO)
+library(CellSIUS)
 library(Seurat)
 library(dplyr)
 
@@ -31,26 +31,41 @@ dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 ## Load Seurat object
 ## -----------------------------
 load(input_file)
-        
+
 P1.seurat.obj <- NormalizeData(P1.seurat.obj) %>% 
   FindVariableFeatures() %>% 
   ScaleData() %>% 
   RunPCA() %>% 
-  RunUMAP(reduction = "pca",dims = 1:30)
+  FindNeighbors() %>%
+  FindClusters()
+
+norm.counts    <- P1.seurat.obj@assays$RNA@data %>% as.matrix()
+cluster        <- P1.seurat.obj$seurat_clusters %>% as.character()
+names(cluster) <- colnames(P1.seurat.obj)
 
 
 ## -----------------------------
-## Run aKNNO
+## Run CellSIUS
 ## -----------------------------
-start_time    <- Sys.time()
-P1.seurat.obj <- FindNeighbors_aKNNO(P1.seurat.obj,verbose = F)
-P1.seurat.obj <- FindClusters(P1.seurat.obj,graph.name="aKNN_O",verbose=F)
-end_time      <- Sys.time()
-        
+start_time     <- Sys.time()
+CellSIUS.out   <- CellSIUS(mat.norm = norm.counts, 
+                           group_id = cluster, 
+                           min_n_cells = 10, 
+                           min_fc = 2,
+                           corr_cutoff = NULL, 
+                           iter = 0, 
+                           max_perc_cells = 50,
+                           fc_between_cutoff = 1, 
+                           mcl_path = "/home/jinxiuyuan/mcl/local/bin/mcl")
+end_time       <- Sys.time()
+
+
 comput_time   <- as.numeric(difftime(end_time, start_time, units = "secs")) / 60
-        
-final_cluster  <- P1.seurat.obj$aKNN_O_res.0.8 %>% as.data.frame()
+
+Result_List    <- CellSIUS_GetResults(CellSIUS.out = CellSIUS.out)
+final_cluster  <- CellSIUS_final_cluster_assignment(CellSIUS.out, group_id = cluster, min_n_genes = 3) %>% as.data.frame()
 colnames(final_cluster) <- "cluster"
+rownames(final_cluster) <- colnames(P1.seurat.obj)
 
 
 ## -----------------------------
@@ -58,20 +73,20 @@ colnames(final_cluster) <- "cluster"
 ## -----------------------------      
 rare.cell      <- colnames(subset(P1.seurat.obj, cell_type %in% clusters[[Celltype]]))
 major.cell     <- setdiff(colnames(P1.seurat.obj),rare.cell)
-        
+
 rare.clst      <- names(table(final_cluster[rownames(final_cluster) %in% rare.cell, ]))[which.max(table(final_cluster[rownames(final_cluster) %in% rare.cell, ]))]
-aKNNO.clst     <- subset(final_cluster, cluster %in% rare.clst)
-aKNNO.major    <- subset(final_cluster, cluster != rare.clst)
-        
-TP <- length(intersect(rownames(aKNNO.clst),rare.cell))
-FP <- length(intersect(rownames(aKNNO.clst),major.cell))      
-TN <- length(intersect(rownames(aKNNO.major),major.cell))
-FN <- length(intersect(rownames(aKNNO.major),rare.cell))
-        
+CellSius.clst  <- subset(final_cluster, cluster %in% rare.clst)
+CellSius.major <- subset(final_cluster, cluster != rare.clst)
+
+TP <- length(intersect(rownames(CellSius.clst),rare.cell))
+FP <- length(intersect(rownames(CellSius.clst),major.cell))      
+TN <- length(intersect(rownames(CellSius.major),major.cell))
+FN <- length(intersect(rownames(CellSius.major),rare.cell))
+
 precision <- TP/(TP+FP)
 recall    <- TP/(TP+FN)
 F1.score  <- (2*precision*recall)/(precision+recall)
-        
+
 result <- data.frame(
   Experiment = experimentID,
   NumRare = length(rare.cell),
@@ -88,9 +103,9 @@ result <- data.frame(
 
 rownames(result) <- experimentID        
 
-  
+
 ## -----------------------------
 ## Save result
 ## -----------------------------
-write.csv(result,file = paste0(output_dir, "/aKNNO_", Celltype, ".csv"),row.names = TRUE)
+write.csv(result,file = paste0(output_dir, "/CellSIUS_", Celltype, ".csv"),row.names = TRUE)
 
