5dd684eca3061fb2e1e145e6df8927e1b80e2b6d
XiuyuanJin <97435279+XiuyuanJin@users.noreply.github.com>
2026-07-29 11:28:49 +0800

    Add files via upload

---
+++ code.for.benchmarking/run_SCISSORS.R
@@ -0,0 +1,110 @@
+
+setwd("/home/jinxiuyuan/Proj_scCellFishing/")
+
+library(dplyr)
+library(Seurat)
+library(SCISSORS)
+library(future)
+
+## -----------------------------
+## Demo parameters
+## -----------------------------
+SampleID  <- "PBMC3k"
+Celltype  <- "Naive"
+clusters  <- list(Naive = "Naive CD4 T")
+
+PrecRare <- 0.05
+DatasetID <- 1
+
+## Example experiment ID:
+## Naive.0.05.1
+experimentID <- paste(Celltype, PrecRare, DatasetID, sep = ".")
+print(experimentID)
+
+
+input_file <- paste0("data/", SampleID, "/",experimentID, ".seurat.obj.RData")
+output_dir <- paste0("output/", SampleID, "/", Celltype)
+
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+
+
+## -----------------------------
+## Load Seurat object
+## -----------------------------
+load(input_file)
+
+
+## -----------------------------
+## Run aKNNO
+## -----------------------------
+start_time    <- Sys.time()
+
+P1.seurat.obj <- PrepareData(P1.seurat.obj, n.HVG = 4000, n.PC = 15, which.dim.reduc = "umap",  = 629)
+sil_score_df <- ComputeSilhouetteScores(P1.seurat.obj, avg = FALSE)
+        
+cluster_avg <- sil_score_df %>% group_by(Cluster) %>% summarise(AvgScore = mean(Score))
+overall_avg <- mean(cluster_avg$AvgScore)
+which.clust <- cluster_avg %>% filter(AvgScore < overall_avg) %>% pull(Cluster) %>% as.character() %>% as.numeric()         
+        
+plan(sequential)
+t_reclust <- ReclusterCells(P1.seurat.obj, 
+                            which.clust = which.clust, 
+                            merge.clusters = TRUE, 
+                            k.vals = c(30, 40, 50), 
+                            resolution.vals = c(.2, .3, .4), 
+                            n.HVG = 4000, 
+                            n.PC = 15, 
+                            redo.embedding = TRUE, 
+                            use.parallel = FALSE,
+                            random.seed = 312)
+        
+P1.seurat.obj <- IntegrateSubclusters(P1.seurat.obj, reclust.results = t_reclust)
+
+end_time       <- Sys.time()
+comput_time    <- as.numeric(difftime(end_time, start_time, units = "secs")) / 60
+        
+final_cluster  <- FetchData(P1.seurat.obj,  vars = "seurat_clusters") %>% as.data.frame()
+colnames(final_cluster) <- "cluster"
+rownames(final_cluster) <- colnames(P1.seurat.obj)
+
+## -----------------------------
+## Calculate performance metrics
+## -----------------------------      
+rare.cell      <- colnames(subset(P1.seurat.obj, cell_type %in% clusters[[Celltype]]))
+major.cell     <- setdiff(colnames(P1.seurat.obj),rare.cell)
+
+rare.clst <- names(table(final_cluster[rownames(final_cluster) %in% rare.cell, ]))[which.max(table(final_cluster[rownames(final_cluster) %in% rare.cell, ]))]
+SCISSORS.clst  <- subset(final_cluster, cluster %in% rare.clst)
+SCISSORS.major <- subset(final_cluster, cluster != rare.clst)
+
+TP <- length(intersect(rownames(SCISSORS.clst),rare.cell))
+FP <- length(intersect(rownames(SCISSORS.clst),major.cell))      
+TN <- length(intersect(rownames(SCISSORS.major),major.cell))
+FN <- length(intersect(rownames(SCISSORS.major),rare.cell))
+
+precision <- TP/(TP+FP)
+recall    <- TP/(TP+FN)
+F1.score  <- (2*precision*recall)/(precision+recall)
+        
+result <- data.frame(
+  Experiment = experimentID,
+  NumRare = length(rare.cell),
+  PrecRare = PrecRare,
+  Time = compute_time,
+  precision = precision,
+  TP = TP,
+  FP = FP,
+  TN = TN,
+  FN = FN,
+  recall = recall,
+  F1 = F1.score
+)
+
+rownames(result) <- experimentID        
+
+
+## -----------------------------
+## Save result
+## -----------------------------
+write.csv(result,file = paste0(output_dir, "/SCISSORS_", Celltype, ".csv"),row.names = TRUE)
+
